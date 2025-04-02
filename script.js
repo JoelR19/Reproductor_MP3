addf525d8595b78c27ef75239c83d003006c71c1
joelR19 <restrepojoel16@gmail.com>
2025-04-02 16:02:07 -0500

    Mejora del diseño y funcionalidad de los botones de adelante y atras V2

--- script.js
+++ script.js
@@ -9,26 +9,34 @@ const pictures = ['./imgs/wosVSaczino.avif', './imgs/interestellar.jpeg', './img
 const music = document.getElementById('song');
 const image = document.getElementById('picture');
 const Name = document.getElementById('title');
+const btnToggle = document.getElementById('togglePlay');
 let position = 0;
 
 function change() {
   music.src = songs[position];
   image.src = pictures[position];
   Name.texContent = titles[position];
-  music.play();
 }
 change();
 
 function togglePlay() {
   if (music.paused) {
     music.play();
+    btnToggle.src = './imgs/pausa.png';
   } else {
     music.pause();
+    btnToggle.src = './imgs/play.png';
   }
 }
 
 function nextSong() {
-  position++;
+  position = (position + 1) % songs.length;
+  change();
+  music.play();
 }
 
-function prevSong() {}
+function prevSong() {
+  position = (position - 1 + songs.length) % songs.length;
+  change();
+  music.play();
+}
